ff984ee784df07af5ed094ff4c9941c2f621e10a
ruben wilson <rwilson_connell@yahoo.com>
2022-08-25 12:25:47 +0100

    finished weather api

--- phasetwo/weather-app/weatherApp.js
+++ phasetwo/weather-app/weatherApp.js
@@ -4,26 +4,34 @@ const Api = require('./weatherApi');
 class Weather{
   constructor(api){
     this.api = api;
+    this.weather = null;
   }
 
   fetch(location){
     this.api.fetchWeatherData(location, (response)=>{
-      console.log(response);
-      // let weather_data = JSON.parse(response.body)
-      // console.log(weather_data);
+      // console.log(response.body);
+      let weather_data = JSON.parse(response.body)
+      this.weather = weather_data;
     });
   }
 
   async getWeatherData(){
-    let weather_data = await this.fetch()
-    // console.log(weather_data)
+    setTimeout(()=>{
+      console.log(this.weather);
+    }, 1000) 
   }
 }
 
 module.exports = Weather;
 
 
-const api = new Api
+const api = new Api;
 const weather = new Weather(api);
 weather.fetch('London');
 weather.getWeatherData();
+
+
+// weather.fetchWeatherData('London', (response)=>{
+//   let weather_data = JSON.parse(response.body)
+//   console.log(weather_data);
+// });
